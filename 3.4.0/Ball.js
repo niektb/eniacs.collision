4f3baf17c95e495afe786128b03316afae58cf31
Chiel92 <ctje92@gmail.com>
2012-07-12 21:05:04 +0200

    The show and hide methods are updated. Now busy with moving the balls.

--- 3.4.0/Ball.js
+++ 3.4.0/Ball.js
@@ -2,28 +2,41 @@
  * - method to add a tag to the html code
  * - method to remove the tag from the html code
  **/
-function Ball() {
-	this.X=0;
-	this.Y=0;
-	this.dX=0;
+function Ball(x,y) {
+	this.X=x;
+	this.Y=y;
+	this.dX=1;
 	this.dY=0;
 	this.ballId= "undefined";
+	this.tag;
 
 	Ball.prototype.Update = function() {
-		X = X + dX;
-		Y = Y + dY;
+		this.X = this.X + this.dX;
+		this.Y = this.Y + this.dY;
+
+		// Update the html attributes
+		//this.tag.setAttribute("top", this.Y);
+		//this.tag.setAttribute("left", this.X);
+		//this.tag.top = this.Y;
+		//this.tag.left = this.X;
 	}
 
-	Ball.prototype.AddToHTML = function() {
+	Ball.prototype.Show = function() {
 		var wrapper = document.getElementById("balls");
 		var balls = wrapper.getElementsByTagName("img");
-		var ballNr = parseInt(balls[balls.length-1].id.substr(4)) + 1;
+
+		// create a unique id for the img element
+		if (balls.length == 0)
+			var ballNr = 1;
+		else
+			var ballNr = parseInt(balls[balls.length-1].id.substr(4)) + 1;
 		this.ballId = "ball" + ballNr;
-		var tag = '<img id="' + this.ballId + '" src="images/green.png" style="position:absolute; top: 100px; left:110px;"/>';
-		wrapper.innerHTML += tag;
+
+		this.tag = '<img id="' + this.ballId + '" src="images/green.png" style="position:absolute; top:' + this.Y + 'px; left:' + this.X + 'px;"/>';
+		wrapper.innerHTML += this.tag;
 	}
 	
-	Ball.prototype.RemoveFromHTML = function() {
+	Ball.prototype.Hide = function() {
 		var wrapper = document.getElementById("balls");
 		var tag = wrapper.childNodes[this.ballId];
 		wrapper.removeChild(tag);
