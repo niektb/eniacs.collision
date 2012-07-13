4a531c20ab49f5501396cd75ecdb07b6bcaf3fb9
Chiel92 <ctje92@gmail.com>
2012-07-13 16:20:17 +0200

    userinteraction w.r.t. movement is working

--- 3.4.0/Ball.js
+++ 3.4.0/Ball.js
@@ -1,29 +1,64 @@
-/* TODO:
- * - method to add a tag to the html code
- * - method to remove the tag from the html code
- **/
-function Ball(x,y) {
-	this.X=x;
-	this.Y=y;
-	this.dX=0;
-	this.dY=0;
-	this.ballId= "undefined";
-	this.tag;
+function Ball(x,y,imgSrc) {
+	var X = x; // Current x and y coords
+	var Y = y; 
+	var dX = 0; // Movement per frame in x or y direction
+	var dY = 0;
+	var targetX = 0; // Coords of target
+	var targetY = 0;
+	var speed = 0; // Speed for moving towards a target
+	var hasTarget = false;
 
-	Ball.prototype.setSpeed = function(dx,dy) {
-		this.dX = dx;
-		this.dY = dy;
+	this.id;
+	this.dom;
+	this.imgSrc = imgSrc;
+
+	Ball.prototype.getTarget = function() {
+		return "(" + targetX + "," + targetY + ")";
+	}
+
+	Ball.prototype.getPos = function() {
+		return "(" + X + "," + Y + ")";
+	}
+
+	Ball.prototype.setDirection = function(dx,dy) {
+		dX = dx;
+		dY = dy;
+	}
+
+	Ball.prototype.setSpeed = function(s) {
+		speed = s;
+	}
+
+	Ball.prototype.setTarget = function(x,y) {
+		hasTarget = true;
+		targetX = x;
+		targetY = y;
 	}
 
 	Ball.prototype.Update = function() {
-		this.X += this.dX;
-		this.Y += this.dY;
+		if (hasTarget) {
+			dX = targetX - X;
+			dY = targetY - Y;
+		}
 
-		// Update the html attributes
-		this.tag.style.top = this.Y + 'px';
-		this.tag.style.left = this.X + 'px';
+		// Scale (dX,dY) to have the right speed
+		if (!(dX == 0 && dY == 0)) {
+			var scalar = speed / Math.sqrt(dX*dX + dY*dY);
+			if (hasTarget && scalar < 1) {
+				dX *= scalar;
+				dY *= scalar;
+			}
+		}
+
+		X += dX;
+		Y += dY;
+
+		// Update the actual dom properties
+		this.dom.style.top = Math.round(Y) + 'px';
+		this.dom.style.left = Math.round(X) + 'px';
 	}
 
+	// Add this ball to DOM
 	Ball.prototype.Show = function() {
 		var wrapper = document.getElementById("balls");
 		var balls = wrapper.getElementsByTagName("img");
@@ -33,17 +68,18 @@ function Ball(x,y) {
 			var ballNr = 1;
 		else
 			var ballNr = parseInt(balls[balls.length-1].id.substr(4)) + 1;
-		this.ballId = "ball" + ballNr;
+		this.id = "ball" + ballNr;
 
-		var html = '<img id="' + this.ballId + '" src="images/green.png" style="position:absolute; top:' + this.Y + 'px; left:' + this.X + 'px;"/>';
+		var html = '<img id="' + this.id + '" src="' + this.imgSrc + '" style="position:absolute; top:' + Y + 'px; left:' + X + 'px;"/>';
 		wrapper.innerHTML += html;
-		this.tag = wrapper.childNodes[this.ballId];
+		this.dom = wrapper.childNodes[this.id];
 	}
 	
+	// Rmove ball from DOM
 	Ball.prototype.Hide = function() {
 		var wrapper = document.getElementById("balls");
-		var tag = wrapper.childNodes[this.ballId];
-		wrapper.removeChild(tag);
+		var dom = wrapper.childNodes[this.id];
+		wrapper.removeChild(dom);
 	}
 }
 
